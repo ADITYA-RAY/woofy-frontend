7ecd9cc17de8aa562a6cb2ddf76f19283de02aca
lakshmanmulchandani <74480329+lakshmanmulchandani@users.noreply.github.com>
2021-07-06 20:23:04 -0700

    navbar-js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const mainMenu = document.querySelector(".mainMenu");
+var closeMenu = document.querySelector(".closeMenu");
+const openMenu = document.querySelector(".openMenu");
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+
+function show() {
+  mainMenu.style.display = "flex";
+  mainMenu.style.top = "0";
+}
+
+function close() {
+  mainMenu.style.top = "-100%";
+}
 
