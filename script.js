f6bc6018b9a3ca87f1543c5836f59ec7bfb004b4
ADITYA-RAY <74045024+ADITYA-RAY@users.noreply.github.com>
2021-07-07 14:13:42 +0530

    login/registration ui added

--- script.js
+++ script.js
@@ -13,3 +13,41 @@ function show() {
 function close() {
   mainMenu.style.top = "-120%";
 }
+
+function openlogin(){
+  document.getElementsByClassName("herotxt")[0].style.opacity="0";
+  document.getElementsByClassName("loginwrap")[0].style.opacity="1";
+  document.getElementsByClassName("loginwrap")[0].style.display="block";
+  document.getElementsByClassName("hero")[0].style.opacity="0.3";
+  
+}
+
+function closelogin(){
+  document.getElementsByClassName("herotxt")[0].style.opacity="1";
+  document.getElementsByClassName("loginwrap")[0].style.opacity="0";
+  document.getElementsByClassName("loginwrap")[0].style.display="none";
+  document.getElementsByClassName("hero")[0].style.opacity="1";
+  
+}
+
+function opensignup(){
+  document.getElementsByClassName("loginwrap")[0].style.opacity="0";
+  document.getElementsByClassName("loginwrap")[0].style.display="none";
+  document.getElementsByClassName("signupwrap")[0].style.opacity="1";
+  document.getElementsByClassName("signupwrap")[0].style.display="block";
+  
+}
+function closesignup(){
+  document.getElementsByClassName("herotxt")[0].style.opacity="1";
+  document.getElementsByClassName("signupwrap")[0].style.opacity="0";
+  document.getElementsByClassName("signupwrap")[0].style.display="none";
+  document.getElementsByClassName("hero")[0].style.opacity="1";
+}
+
+function opensignin(){
+  document.getElementsByClassName("loginwrap")[0].style.opacity="1";
+  document.getElementsByClassName("loginwrap")[0].style.display="block";
+  document.getElementsByClassName("signupwrap")[0].style.opacity="0";
+  document.getElementsByClassName("signupwrap")[0].style.display="none";
+  
+}
